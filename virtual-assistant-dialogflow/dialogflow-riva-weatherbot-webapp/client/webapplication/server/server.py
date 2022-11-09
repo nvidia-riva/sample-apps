67b8fe5541fc8e647787c89a8cdac2d4bd736454
Sven Chilton <sven.chilton@gmail.com>
2022-11-09 15:22:55 -0800

    Minor changes to Flash server script
    Signed-off-by: Sven Chilton <sven.chilton@gmail.com>

--- virtual-assistant-dialogflow/dialogflow-riva-weatherbot-webapp/client/webapplication/server/server.py
+++ virtual-assistant-dialogflow/dialogflow-riva-weatherbot-webapp/client/webapplication/server/server.py
@@ -9,28 +9,26 @@ from __future__ import division
 
 import uuid
 import time
-import logging
-from flask import Flask, jsonify, send_from_directory, Response, request
+from flask import Flask, jsonify, send_from_directory, Response, request, logging
 from flask_cors import CORS
 from flask import stream_with_context
 from flask_socketio import SocketIO, emit
 from os.path import dirname, abspath, join, isdir
 from os import listdir
+from config import client_config
 from engineio.payload import Payload
 
-from config import client_config
-from riva.chatbot.chatbots_multiconversations_management import create_chatbot, get_new_user_conversation_index, get_chatbot
+from riva_local.chatbot.chatbots_multiconversations_management import create_chatbot, get_new_user_conversation_index, get_chatbot
 
 ''' Flask Initialization 
 '''
 app = Flask(__name__)
 cors = CORS(app)
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
+log = logging.logging.getLogger('werkzeug')
+log.setLevel(logging.logging.ERROR)
 Payload.max_decode_packets = 500  # https://github.com/miguelgrinberg/python-engineio/issues/142
 sio = SocketIO(app, logger=False)
 verbose = client_config['VERBOSE']
-VA_INIT_MESSAGE = "Hi, welcome to RIVA weather service. How can I help you?"
 
 # Methods to show client
 @app.route('/rivaWeather/')
@@ -51,7 +49,7 @@ def get_newuser_conversation_index():
 @app.route('/audio/<int:user_conversation_index>/<int:post_id>')
 def audio(user_conversation_index, post_id):
     if verbose:
-        print(f'[Client Server] [{user_conversation_index}] audio speak: {post_id}')
+        print(f'[{user_conversation_index}] audio speak: {post_id}')
     currentChatbot = get_chatbot(user_conversation_index)
     return Response(currentChatbot.get_tts_speech())
 
@@ -71,42 +69,38 @@ def stream(user_conversation_index):
 
 
 # Used for sending messages to the bot
-@app.route( "/init", methods=['POST'])
-def init():
-    try:
-        user_conversation_index = request.json['user_conversation_index']
-    except KeyError:
-        return jsonify(ok=False, message="Missing parameters.")
-    try:
-        create_chatbot(user_conversation_index, sio)  
-        return jsonify(ok=True, messages=[VA_INIT_MESSAGE], debug=client_config["DEBUG"])
-    except Exception as e:  # Error in execution
-        print("[Client Server] " + str(e))
-        return jsonify(ok=False, message="Error during execution.")
-    
-
-# Used for sending messages to the bot
 @app.route( "/", methods=['POST'])
 def get_input():
     try:
         text = request.json['text']
+        context = request.json['context']
         bot = request.json['bot'].lower()
+        payload = request.json['payload']
         user_conversation_index = request.json['user_conversation_index']
     except KeyError:
         return jsonify(ok=False, message="Missing parameters.")
     if user_conversation_index:
-        create_chatbot(user_conversation_index, sio)
+        create_chatbot(user_conversation_index, sio, verbose=client_config['VERBOSE'])
         currentChatBot = get_chatbot(user_conversation_index)
         try:
-            response_text = currentChatBot.dialogflow_tts_pipeline(text)    
-            if verbose:
-                print(f"[Client Server] [{user_conversation_index}] Response from Dialogflow: {response_text}")
-            return jsonify(ok=True, messages=[response_text], debug=client_config["DEBUG"])
+            response = currentChatBot.stateDM.execute_state(
+                bot, context, text)
+
+            if client_config['DEBUG']:
+                print(f"[{user_conversation_index}] Response from RivaDM: {response}")
+            
+            for resp in response['response']:
+                speak = resp['payload']['text']
+                if len(speak):
+                    currentChatBot.tts_fill_buffer(speak)
+            return jsonify(ok=True, messages=response['response'], context=response['context'],
+                           session=user_conversation_index, debug=client_config["DEBUG"])
         except Exception as e:  # Error in execution
-            print("[Client Server] " + str(e))
+
+            print(e)
             return jsonify(ok=False, message="Error during execution.")
     else:
-        print("[Client Server] user_conversation_index not found")
+        print("user_conversation_index not found")
         return jsonify(ok=False, message="user_conversation_index not found")
 
 
@@ -137,7 +131,7 @@ def pauseASR(data):
     currentChatbot = get_chatbot(data["user_conversation_index"])
     if currentChatbot:
         if verbose:
-            print(f"[Client Server] [{data['user_conversation_index']}] Pausing ASR requests.")
+            print(f"[{data['user_conversation_index']}] Pausing ASR requests.")
         currentChatbot.pause_asr()
         
 
@@ -146,7 +140,7 @@ def unpauseASR(data):
     currentChatbot = get_chatbot(data["user_conversation_index"])
     if currentChatbot:
         if verbose:
-            print(f"[Client Server] [{data['user_conversation_index']}] Attempt at Unpausing ASR requests on {data['on']}.")
+            print(f"[{data['user_conversation_index']}] Attempt at Unpausing ASR requests on {data['on']}.")
         unpause_asr_successful_flag = currentChatbot.unpause_asr(data["on"])
         if unpause_asr_successful_flag == True:
             emit('onCompleteOf_unpause_asr', {'user_conversation_index': data["user_conversation_index"]}, broadcast=False)
@@ -163,10 +157,10 @@ def pause_wait_unpause_asr(data):
 @sio.on("connect", namespace="/")
 def connect():
     if verbose:
-        print('[Client Server] Client connected')
+        print('[Riva Chatbot] Client connected')
 
 
 @sio.on("disconnect", namespace="/")
 def disconnect():
     if verbose:
-        print('[Client Server] Client disconnected')
+        print('[Riva Chatbot] Client disconnected')
